1dc37be5e381e027fb25c827909a6cd434760e98
HAMP <hmendo81@gmail.com>
2016-06-10 14:51:49 -0500

    Diseño de Activitys Login, Deposito y Retiro

---
+++ Sesion 04/AndroidMysql/AppUPAOBank/app/src/main/java/com/example/upao/appupaobank/DepositoActivity.java
@@ -0,0 +1,20 @@
+package com.example.upao.appupaobank;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+public class DepositoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deposito);
+    }
+
+}
---
+++ Sesion 04/AndroidMysql/AppUPAOBank/app/src/main/java/com/example/upao/appupaobank/RetiroActivity.java
@@ -0,0 +1,21 @@
+package com.example.upao.appupaobank;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+public class RetiroActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retiro);
+    }
+}
