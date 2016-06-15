900615859844473e7163e39d5b252e09e87dcf4b
yocom <you@example.com>
2016-06-15 18:40:38 -0500

    Implementacion de ListartvShows y RegistroTvshows

--- Sesion 05/AndroidRetrofit2/app/src/main/java/com/example/upao/androidretrofit/ListarTvshows.java
+++ Sesion 05/AndroidRetrofit2/app/src/main/java/com/example/upao/androidretrofit/ListarTvshows.java
@@ -1,23 +1,69 @@
 package com.example.upao.androidretrofit;
 
 import android.app.Activity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.upao.androidretrofit.apiservices.RestClient;
+import com.example.upao.androidretrofit.apiservices.model.Tvshow;
+
 
 public class ListarTvshows  extends Fragment {
+
+    List<String> tvshowslist = new ArrayList<>();
+
+    private ListAdapter mAdapter;
+
+    RestClient restClient = RestClient.getInstance();
+
+    RestClient.ResultReadyCallback callback = new RestClient.ResultReadyCallback() {
+        @Override
+        public void resultReady(List<Tvshow> tvlist) {
+            for(Tvshow tv: tvlist) {
+                tvshowslist.add(tv.getTitulo());
+            }
+
+            ((ArrayAdapter) mAdapter).notifyDataSetChanged();
+        }
+    };
+
+
+    public static ListarTvshows newInstance() {
+        ListarTvshows fragment = new ListarTvshows();
+        return fragment;
+    }
+
+    public ListarTvshows() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        mAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, tvshowslist);
+
+        restClient.setCallback(callback);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_item, container, false);
+
+        AbsListView mListView = (AbsListView) view.findViewById(android.R.id.list);
+        ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
 
 
         return view;
@@ -26,7 +72,7 @@ public class ListarTvshows  extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-
+        restClient.getTvshows();
     }
 
     @Override
--- Sesion 05/AndroidRetrofit2/app/src/main/java/com/example/upao/androidretrofit/RegistroTvshow.java
+++ Sesion 05/AndroidRetrofit2/app/src/main/java/com/example/upao/androidretrofit/RegistroTvshow.java
@@ -1,12 +1,39 @@
 package com.example.upao.androidretrofit;
 
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegistroTvshow extends Fragment {
+import com.example.upao.androidretrofit.apiservices.RestClient;
+import com.example.upao.androidretrofit.apiservices.model.Tvshow;
+
+public class RegistroTvshow  extends Fragment {
+
+    EditText titulo;
+    EditText anno;
+    EditText pais;
+    EditText temporadas;
+    EditText genero;
+    EditText resumen;
+    Button addTvshow;
+    Button retTvshow;
+    RestClient restClient = RestClient.getInstance();
+
+
+    public static RegistroTvshow newInstance() {
+        return new RegistroTvshow();
+    }
+
+    public RegistroTvshow() {
+        // Required empty public constructor
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,8 +44,42 @@ public class RegistroTvshow extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.activity_registro_tvshow, container, false);
+
+        titulo = (EditText) root.findViewById(R.id.etTitulo);
+        anno = (EditText) root.findViewById(R.id.etAnno);
+        pais = (EditText) root.findViewById(R.id.etPais);
+        temporadas = (EditText) root.findViewById(R.id.etTemporadas);
+        genero = (EditText) root.findViewById(R.id.etGenero);
+        resumen = (EditText) root.findViewById(R.id.etResumen);
+        addTvshow = (Button) root.findViewById(R.id.btnRegistrar);
+
+
+
+        addTvshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String _titulo = titulo.getText().toString();
+                int _anno = Integer.parseInt(anno.getText().toString());
+                String _pais=pais.getText().toString();
+                int _temporadas=Integer.parseInt(temporadas.getText().toString());
+                String _genero=genero.getText().toString();
+                String _resumen=resumen.getText().toString();
+
+                Tvshow tv = new Tvshow(_titulo,_anno,_pais,_temporadas,_genero,_resumen);
+                restClient.createTvshow(getActivity(), tv);
+                titulo.setText("");
+                anno.setText("");
+                pais.setText("");
+                temporadas.setText("");
+                genero.setText("");
+                resumen.setText("");
+
+            }
+        });
+
         return root;
     }
+
+
 }
