01c1ffe2058d287a3f45fb0dffbf58f318e8225f
yocom <you@example.com>
2016-06-15 18:13:30 -0500

    Proyecto Inicio Android Retrofit

---
+++ Sesion 05/AndroidRetrofit2/app/src/main/java/com/example/upao/androidretrofit/ListarTvshows.java
@@ -0,0 +1,42 @@
+package com.example.upao.androidretrofit;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListarTvshows  extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
---
+++ Sesion 05/AndroidRetrofit2/app/src/main/java/com/example/upao/androidretrofit/RegistroTvshow.java
@@ -0,0 +1,24 @@
+package com.example.upao.androidretrofit;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegistroTvshow extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.activity_registro_tvshow, container, false);
+        return root;
+    }
+}
