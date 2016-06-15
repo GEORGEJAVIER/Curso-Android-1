076b23e6bd3eb6bfa117141b3e64e5556d07f489
yocom <you@example.com>
2016-06-15 18:43:05 -0500

    Implementacion de MainActivity

--- Sesion 05/AndroidRetrofit2/app/src/main/java/com/example/upao/androidretrofit/MainActivity.java
+++ Sesion 05/AndroidRetrofit2/app/src/main/java/com/example/upao/androidretrofit/MainActivity.java
@@ -1,38 +1,79 @@
 package com.example.upao.androidretrofit;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends FragmentActivity {
+
+    UserFragmentPagerAdapter mPagerAdapter;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPagerAdapter = new UserFragmentPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+
+    public class UserFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final String[] TITLES = {"Registro TvShow", "Listar TvShow"};
+
+        public UserFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0:
+                    return RegistroTvshow.newInstance();
+                case 1:
+                    return ListarTvshows.newInstance();
+                default:
+                    return null;
+            }
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 
 }
